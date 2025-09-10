ba8a96df0f632adcf67f95f1158ea8a7471327ab
WillCode2501 <wmartialw@gmail.com>
2025-09-10 10:40:24 -0400

    Add files via upload

---
+++ Week 2/Vector Note.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+//	vector<int> numbers = {10, 20, 30, 40, 50};
+//	// Get values from vector
+//	int firstElement = numbers.front(); // First element of the vector
+//	int secondElement = numbers[1]; // Find the second element of the vector
+//	int thirdElement = numbers.at(2); // Find and check if there is a third element, if not then exception
+//	int lastElement = numbers.back(); // Find the last element of the vector
+//	
+//	// Display values to the user
+//	cout<<"firstElement: "<< firstElement<< endl; // Outputs 10
+//	cout<<"secondElement: "<< secondElement<< endl; // Outputs 20
+//	cout<<"thirdElement: "<< thirdElement<< endl; // Outputs 30
+//	cout<<"lastElement: "<< lastElement<< endl; // Outputs 50
+//	
+//	// vector modification
+//	numbers[1] = 35;  // 10, 35, 30, 40, 50
+//	numbers.at(2) = 35; // 10, 35, 35, 40, 50
+//	numbers.front() = 5; // 5, 35, 35, 40, 5
+//	numbers.back() = 5; // 5, 35, 35, 40, 5
+//	numbers.push_back(272); // 5, 35, 35, 40, 5, 272
+//
+//	cout << "\n\n======After Modification======\n\n";
+//	
+//	cout<<"firstElement: "<< numbers.front() << endl; 
+//	cout<<"secondElement: "<< numbers[1] << endl;
+//	cout<<"thirdElement: "<< numbers.at(2) << endl; 
+//	cout<<"lastElement: "<< numbers.back() << endl;
+	
+	
+	
+//	vector <int> numbers;
+//	
+//	// Preallocate memory for 3 elements (capacity = 3)
+//	numbers.reserve(2); // nothing in vector, so it print from memory
+//	cout << "First E1 = " << numbers.front() << endl;
+//	cout << "Last E1 = " << numbers.back() << endl << endl;
+//	cout << "Reserved: " << endl << "Size = " << numbers.size()
+//	<< ", Capacity = " << numbers.capacity() << endl << endl;
+//	
+//	numbers.push_back(10);
+//	cout << "After adding 10: " << endl << "Size = " << numbers.size()
+//	<< ", Capacity = " << numbers.capacity() << endl;
+//	// both first and last value is 10 because it's the only element
+//	cout << "First E1 = " << numbers.front() << endl;
+//	cout << "Last E1 = " << numbers.back() << endl << endl;
+//
+//	numbers.push_back(20);
+//	cout << "After adding 20: " << endl << "Size = " << numbers.size()
+//	<< ", Capacity = " << numbers.capacity() << endl;
+//	cout << "First E1 = " << numbers.front() << endl;
+//	cout << "Last E1 = " << numbers.back() << endl << endl;
+//
+//	numbers.push_back(40);
+//	cout << "After adding 40: " << endl << "Size = " << numbers.size()
+//	<< ", Capacity = " << numbers.capacity() << endl;
+//	// capacity is raised to 4 because reserve(2) increase by increment of 2 when passing the capacity
+//	cout << "First E1 = " << numbers.front() << endl;
+//	cout << "Last E1 = " << numbers.back() << endl << endl;
+	
+	
+	return 0;
+}
