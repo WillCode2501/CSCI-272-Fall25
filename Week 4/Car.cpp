a1f6265838e1e11e36116a1dbb15820e2508da32
WillCode2501 <wmartialw@gmail.com>
2025-09-29 10:38:07 -0400

    Update Car.cpp

--- Week 4/Car.cpp
+++ Week 4/Car.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 
 int main() {
-	Car car1; // default constructor
+	Car car1; // create object, default constructor
 	Car car2("Tesla","X", 2022); // parametrized constructor
 	Car car3(car2); //copy Constructor
-	Car* pCar1 = new Car; // default constructor
-	Car* pCar2 = new Car("Tesla","X", 2022); // parametrized constructor
+	
+	Car* pCar1 = new Car; // pointer for default car
+	Car* pCar2 = new Car("Tesla","X", 2022); // pointer for parametrized car
 	Car car[20]; // default constructor
-	car1.startEngine();
-	car2.startEngine();
-	car3.startEngine();
-	pCar1->startEngine();
-	pCar2->startEngine();
-	car[0].startEngine();
+	
+	car1.startEngine(); // Start engine for 2025 Honda Civic
+	car2.startEngine(); // Start engine for 2022 Tesla X
+	car3.startEngine(); // Start engine for 2022 Tesla X
+	
+	pCar1->startEngine(); // Start engine for 2025 Honda Civic
+	pCar2->startEngine(); // Start engine for 2022 Tesla X
+	
+	car[0].startEngine(); // Start engine for 2025 Honda Civic
 	
 	return 0; 
+
 }
